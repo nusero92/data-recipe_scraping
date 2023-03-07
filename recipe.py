ae999d82dc84a4c9076642377790c1dad09513ad
anamaria lukacs <anamarialukacs030@gmail.com>
2023-03-07 11:30:47 +0100

    functions updated

--- recipe.py
+++ recipe.py
@@ -1,28 +1,57 @@
 # pylint: disable=missing-docstring,line-too-long
 import sys
 from os import path
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+SEARCH_URL = "https://recipes.lewagon.com/"
+PAGES_TO_SCRAPE = 3
 
 
 def parse(html):
     ''' return a list of dict {name, difficulty, prep_time} '''
-    pass  # YOUR CODE HERE
+
+    soup = BeautifulSoup(html, "html.parser")
+    return [parse_recipe(article) for article in soup.find_all('div', class_= 'recipe-details')]
+
 
 def parse_recipe(article):
     ''' return a dict {name, difficulty, prep_time} modelising a recipe'''
-    pass  # YOUR CODE HERE
+
+    name = article.find('p', class_= 'recipe-name').string.strip()
+    difficulty = article.find('span', class_= 'recipe-difficulty').string.strip()
+    prep_time = article.find('span', class_= 'recipe-cooktime').string.strip()
+    return {'name': name, 'difficulty': difficulty, 'prep_time': prep_time}
+
 
 def write_csv(ingredient, recipes):
     ''' dump recipes to a CSV file `recipes/INGREDIENT.csv` '''
-    pass  # YOUR CODE HERE
+
+    with open(f'recipes/{ingredient}.csv', 'w',encoding="utf-8") as recipe_file:
+        keys = recipes[0].keys()
+        writer = csv.DictWriter(recipe_file, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(recipes)
+
 
 def scrape_from_internet(ingredient, start=1):
     ''' Use `requests` to get the HTML page of search results for given ingredients. '''
-    pass  # YOUR CODE HERE
+
+    print(f"Scraping page {start}")
+    response = requests.get(
+        SEARCH_URL,
+        params={'search[query]': ingredient, 'page': start}
+        )
+    if response.history:
+        return None
+    return response.text
+
 
 def scrape_from_file(ingredient):
     file = f"pages/{ingredient}.html"
     if path.exists(file):
-        return open(file)
+        return open(file,encoding="utf-8")
     print("Please, run the following command first:")
     print(f'curl -g "https://recipes.lewagon.com/?search[query]={ingredient}" > pages/{ingredient}.html')
     sys.exit(1)
@@ -31,10 +60,18 @@ def scrape_from_file(ingredient):
 def main():
     if len(sys.argv) > 1:
         ingredient = sys.argv[1]
-        # TODO: Replace scrape_from_file with scrape_from_internet and implement pagination (more than 2 pages needed)
+
         recipes = parse(scrape_from_file(ingredient))
         write_csv(ingredient, recipes)
-        pass  # YOUR CODE HERE
+
+        recipes = []
+        for page in range(PAGES_TO_SCRAPE):
+            response = scrape_from_internet(ingredient, page+1)
+            if response:
+                recipes += parse(response)
+            else:
+                break
+        print(f"Wrote recipes to recipes/{ingredient}.csv")
     else:
         print('Usage: python recipe.py INGREDIENT')
         sys.exit(0)
--- test_scraping.py
+++ test_scraping.py
@@ -1,4 +1,8 @@
 # pylint: disable=missing-docstring,invalid-name
+from bs4 import BeautifulSoup
 
-# TODO: paste the code from Kitt's instructions
+with open("pages/carrot.html",encoding="utf-8") as file:
+    soup = BeautifulSoup(file, "html.parser")
 
+for recipe in soup.find_all('p', {"class_": 'recipe-name'}):
+    print(recipe.text)
